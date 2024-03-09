99eb8d29e643ee871b87c9055a4a2680aea59cb1
SlavKoVrn <slavko.chita@gmail.com>
2024-03-09 12:11:08 +0300

    front sort

--- frontend/views/image/_search.php
+++ frontend/views/image/_search.php
@@ -18,10 +18,26 @@ use yii\widgets\ActiveForm;
         ],
     ]); ?>
 
-    <?= $form->field($model, 'name')->label(false) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Поиск', ['class' => 'btn btn-primary']) ?>
+    <div class="row">
+        <div class="col col-sm-5">
+            <?= $form->field($model, 'name')->label(false) ?>
+        </div>
+        <div class="col col-sm-5">
+            <?= Html::dropDownList('sort', [
+                Yii::$app->request->get('sort'),
+            ], [
+                '' => '',
+                'name' => 'По наименованию возрастанию',
+                '-name' => 'По наименованию убыванию',
+                'datetime' => 'По дата и время возрастанию',
+                '-datetime' => 'По дата и время убыванию',
+            ],['class' => 'form-control']) ?>
+        </div>
+        <div class="col col-sm-2">
+            <div class="form-group">
+                <?= Html::submitButton('Поиск', ['class' => 'btn btn-primary']) ?>
+            </div>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
